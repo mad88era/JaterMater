8678d4f4c9dc24507b767832e096cb24adc46a2c
mad88era <150746505+mad88era@users.noreply.github.com>
2024-06-14 14:17:21 -0700

    updated files

--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -3,26 +3,23 @@ const butInstall = document.getElementById('buttonInstall');
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
-    // Store the triggered events
-    event.preventDefault();
+    // stores the tirggered events
     window.deferredPrompt = event;
-
-    // Remove the hidden class from the button.
+    // remove the hidden class fromt he button
     butInstall.classList.toggle('hidden', false);
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
+
     const promptEvent = window.deferredPrompt;
 
     if (!promptEvent) {
         return;
     }
-
-    // Show prompt
+    // shows prompt
     promptEvent.prompt();
-
-    // Reset the deferred prompt variable, it can only be used once.
+    // resets the deferred prompt variable, it can only be used once
     window.deferredPrompt = null;
 
     butInstall.classList.toggle('hidden', true);
@@ -30,6 +27,7 @@ butInstall.addEventListener('click', async () => {
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
-    // Clear prompt
+
+    // clears the prompt
     window.deferredPrompt = null;
 });
--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -1,3 +1,74 @@
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const WebpackPwaManifest = require('webpack-pwa-manifest');
+// const path = require('path');
+// const { InjectManifest } = require('workbox-webpack-plugin');
+
+// module.exports = () => {
+//   return {
+//     mode: 'development',
+//     entry: {
+//       main: './src/js/index.js',
+//       install: './src/js/install.js'
+//     },
+//     output: {
+//       filename: '[name].bundle.js',
+//       path: path.resolve(__dirname, 'dist'),
+//     },
+//     plugins: [
+//       // Webpack plugin that generates HTML file and injects the bundles
+//       new HtmlWebpackPlugin({
+//         template: './index.html',
+//         title: 'jate'
+//       }),
+//       // Custom Service Worker
+//       new InjectManifest({
+//         swSrc: './src-sw.js',
+//         swDest: './src-sw.js'
+//       }),
+//       // Manifest.json file
+//       new WebpackPwaManifest({
+//         fingerprints: false,
+//         inject: true,
+//         name: 'jate',
+//         description: 'A text editor, but be extreme about it',
+//         background_color: '#225ca3',
+//         theme_color: '#00FFFF',
+//         start_url: './',
+//         publicPath: './',
+//         icons: [{
+
+//           src: path.resolve('src/images/logo.png'),
+//           sizes: [96, 128, 192, 256, 384, 512],
+//           destination: path.join('assets', 'icons')
+//         }],
+//       }),
+//     ],
+
+//     module: {
+//       // CSS loaders
+//       rules: [
+//         {
+//           test: /\.css$/i,
+//           use: ['style-loader', 'css-loader'],
+//         },
+//         {
+//           test: /\.m?js$/,
+//           exclude: /node_modules/,
+//           // We use babel-loader in order to use ES6.
+//           use: {
+//             loader: 'babel-loader',
+//             options: {
+//               presets: ['@babel/preset-env'],
+//               plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+//             },
+//           },
+//         },
+
+//       ],
+//     },
+//   };
+// };
+
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
